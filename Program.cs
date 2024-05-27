781d6dfa490a9af1f4ec69e9cdb2a33dffdb0f2a
FerrousInk <moaugustin@icloud.com>
2024-05-27 22:15:06 +0200

    sad

--- Program.cs
+++ Program.cs
@@ -1,51 +1,15 @@
-﻿using System;
-using System.Diagnostics;
-using System.Windows;
+﻿using System.Diagnostics;
 
 namespace AX_SYS
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.Clear();
-            AX_SYS();
-            void AX_SYS ()
-            {
-                Console.WriteLine("AX-SYS Builder V5");
-                Console.WriteLine("");
-                Console.WriteLine("Press 'B' to Build ISO.");
-                Console.WriteLine("Press 'O' to Open 'startnet.cmd'");
-                ConsoleKeyInfo Key = Console.ReadKey();
-                char PressedKey = Key.KeyChar;
-                string PressedKeyString = PressedKey.ToString().ToLower();
-                switch (PressedKeyString)
-                {
-                    case "b":
-                        StartBuildProcess();
-                        break;
-                    case "o":
-                        OpenEditors();
-                        break;
-                }
-                Repeat();
-            }
-            void Repeat ()
-            {
-                Console.Clear();
-                AX_SYS ();
-            }
-        }
-        static void StartBuildProcess()
+        static void Main()
         {
             Process Builder = new Process();
             Builder.StartInfo.FileName = "cmd.exe";
-            Builder.StartInfo.Arguments = "/c pause && exit";
+            Builder.StartInfo.Arguments = "/c build_iso.bat";
             Builder.Start();
-        }
-        static void OpenEditors()
-        {
-            MessageBox.Show("Work In Progress");
         }
     }
 }

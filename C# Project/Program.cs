40f836e7983e28e6e85291e21e18e8871eb00ce5
FerrousInk <139216162+FerrousInk@users.noreply.github.com>
2024-05-30 08:41:14 +0200

    Program.cs aktualisieren

--- C# Project/Program.cs
+++ C# Project/Program.cs
@@ -8,7 +8,9 @@ namespace AX_SYS
     {
         static void Main()
         {
-            string args @=
+            string args @"/c
+
+"
             Process Builder = new Process();
             Builder.StartInfo.FileName = "cmd.exe";
             Builder.StartInfo.Arguments = "/c build_iso.bat";
